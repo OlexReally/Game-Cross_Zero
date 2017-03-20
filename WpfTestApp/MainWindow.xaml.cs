409a84d1791fd9c252b979f59d9d8dc1b65f5440
OlexReally <sector1111@yandex.ru>
2017-03-20 03:55:35 +0200

    cross-zeroes game

--- WpfTestApp/MainWindow.xaml.cs
+++ WpfTestApp/MainWindow.xaml.cs
@@ -20,57 +20,9 @@ namespace WpfTestApp
     /// </summary>
     public partial class MainWindow : Window
     {
-        private Color[] ColorArr;
-        private int[] Pictures;
         public MainWindow()
         {
             InitializeComponent();
-
-        }
-
-        private void Button1_Click(object sender, RoutedEventArgs e)
-        {
-            GetColors();
-            Border1.Background = new SolidColorBrush(ColorArr[new Random().Next(0, ColorArr.Length)]);
-        }
-
-        private void GetColors()
-        {
-            ColorArr = new Color[] {
-                Colors.Black,
-                Colors.Aqua,
-                Colors.GreenYellow,
-                Colors.Indigo,
-                Colors.Beige,
-                Colors.Firebrick
-            };
-        }
-
-        private void Button2Click(object sender, RoutedEventArgs e)
-        {
-            GetPictures();
-            Image1.Source = new BitmapImage(new Uri(@"D:\Pictures\" + new Random().Next(1,4)+ ".jpg"));
-        }
-
-        private void Funcs()
-        {
-
-        }
-
-        private void GetPictures()
-        {
-            Pictures = new int[] {
-                1,
-                2,
-                3,
-                4
-            };
-        }
-
-        private void Button3Click(object sender, RoutedEventArgs e)
-        {
-            Button1_Click(sender, e);
-            Button2Click(sender, e);
         }
     }
 }
