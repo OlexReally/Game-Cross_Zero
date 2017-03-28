078b2cb6235cbf656894b8ab68395c9023799ebe
OlexReally <sector1111@yandex.ru>
2017-03-28 03:54:12 +0300

    add images&button logic

--- WpfTestApp/MainWindow.xaml.cs
+++ WpfTestApp/MainWindow.xaml.cs
@@ -20,9 +20,51 @@ namespace WpfTestApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool check = false;
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private StackPanel getCross()
+        {
+            Image img = new Image();
+            img.Source = new BitmapImage(new Uri("pack://application:,,,/Images/CROSS.jpg"));
+
+            StackPanel stackPnl = new StackPanel();
+            stackPnl.Orientation = Orientation.Horizontal;
+            stackPnl.Margin = new Thickness(1);
+            stackPnl.Children.Add(img);
+
+            return stackPnl;
+        }
+
+        private StackPanel getZero()
+        {
+            Image img = new Image();
+            img.Source = new BitmapImage(new Uri("pack://application:,,,/Images/Zero.jpg"));
+
+            StackPanel stackPnl = new StackPanel();
+            stackPnl.Orientation = Orientation.Horizontal;
+            stackPnl.Margin = new Thickness(1);
+            stackPnl.Children.Add(img);
+
+            return stackPnl;
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+
+            Button b = sender as Button;
+
+            b.IsEnabled = false;
+            if (check)
+                b.Content = getCross();
+            else
+                b.Content = getZero();
+
+            check = !check;
+        }
+
     }
 }
